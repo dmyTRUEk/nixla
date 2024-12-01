aa6688b0efccfe65adddab62d198285bd43a3648
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-12-02 00:24:06 +0200

    feat: enable pipe-operators

--- nixla-nix.sh
+++ nixla-nix.sh
@@ -1,11 +1,11 @@
 FILENAME=$1
 
 # step 1: ensure given file is valid nix expression:
-nix eval --file $FILENAME > /dev/null
+nix --extra-experimental-features pipe-operators eval --file $FILENAME > /dev/null
 
 # step 2: remove filename from all other cli args
 shift 1
 
 # step 3: run nix function on the input:
-nix eval --expr "($(cat $FILENAME)
+nix --extra-experimental-features pipe-operators eval --expr "($(cat $FILENAME)
 ) \"$(echo $@)\""
