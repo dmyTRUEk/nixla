2fd6d890c871e7e30e4e08e124353077360604bb
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-12-02 12:32:56 +0200

    feat: enable imports

--- nixla-nix.sh
+++ nixla-nix.sh
@@ -1,11 +1,22 @@
 FILENAME=$1
 
 # step 1: ensure given file is valid nix expression:
-nix --extra-experimental-features pipe-operators eval --file $FILENAME > /dev/null
+nix \
+	--extra-experimental-features pipe-operators \
+	eval \
+	--impure \
+	--file \
+	$FILENAME \
+	> /dev/null
 
 # step 2: remove filename from all other cli args
 shift 1
 
 # step 3: run nix function on the input:
-nix --extra-experimental-features pipe-operators eval --expr "($(cat $FILENAME)
+nix \
+	--extra-experimental-features pipe-operators \
+	eval \
+	--impure \
+	--expr \
+	"($(cat $FILENAME)
 ) \"$(echo $@)\""
--- nixla.sh
+++ nixla.sh
@@ -1,11 +1,23 @@
 FILENAME=$1
 
 # step 1: ensure given file is valid nix expression:
-nix --extra-experimental-features pipe-operators eval --file $FILENAME > /dev/null
+nix \
+	--extra-experimental-features pipe-operators \
+	eval \
+	--impure \
+	--file \
+	$FILENAME \
+	> /dev/null
 
 # step 2: remove filename from all other cli args
 shift 1
 
 # step 3: run nix function on the input:
-nix --extra-experimental-features pipe-operators eval --raw --expr "($(cat $FILENAME)
+nix \
+	--extra-experimental-features pipe-operators \
+	eval \
+	--impure \
+	--raw \
+	--expr \
+	"($(cat $FILENAME)
 ) \"$(echo $@)\""
